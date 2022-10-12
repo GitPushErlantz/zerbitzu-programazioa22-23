52e741bae7ec56b7fed5d7d30a58bb5d479be8ee
Erlantz <garate.erlantz@uni.eus>
2022-10-12 12:48:48 +0200

    pain and suffering

--- garceta-ariketak/Ariketa16Launcher/src/Main.java
+++ garceta-ariketak/Ariketa16Launcher/src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Runtime r = Runtime.getRuntime();
+        Runtime r = Runtime.getRuntime(); //TODO: runtime erabili
         System.out.println(System.getProperty("user.dir"));
-        String command = "java -jar .../Ariketa16/out/artifacts/garceta_ariketak_jar/garceta-ariketak.jar"; //TODO: Ruta konpondu
-
+        String command = "java -jar ../Ariketa16/out/artifacts/garceta_ariketak_jar/garceta-ariketak.jar 2 4"; //TODO: Ruta konpondu
         try {
-            Process p = new ProcessBuilder(command, "2").start(); //TODO: process erabili
-            p = r.exec(command);
-
-        } catch(Exception e) {
+            ProcessBuilder pb = new ProcessBuilder(command);
+            Process p = pb.start(); //TODO: process erabili
+            //p = r.exec(command);
+        } catch (Exception e) {
             System.out.println("error");
         }
     }

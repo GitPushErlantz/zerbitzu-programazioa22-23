12e7d0ed9167602dc2941ef1b1f1140a800dfa47
Erlantz <garate.erlantz@uni.eus>
2022-10-12 10:26:57 +0200

    fixes

--- garceta-ariketak/Ariketa16Launcher/src/Main.java
+++ garceta-ariketak/Ariketa16Launcher/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Runtime r = Runtime.getRuntime();
+        System.out.println(System.getProperty("user.dir"));
         String command = "java -jar .../Ariketa16/out/artifacts/garceta_ariketak_jar/garceta-ariketak.jar"; //TODO: Ruta konpondu
-        Process p = null; //TODO: process erabili
+
         try {
+            Process p = new ProcessBuilder(command, "2").start(); //TODO: process erabili
             p = r.exec(command);
+
         } catch(Exception e) {
             System.out.println("error");
         }
